04a73618915332f37134a26e3e51a84f21baa3da
tom-villanueva <tomas.villanueva.jousset@gmail.com>
2023-05-20 12:16:39 -0300

    Installed and configured handlebars

--- proyecto_final/app.js
+++ proyecto_final/app.js
@@ -2,10 +2,18 @@ import express from "express";
 import productsRouter from "./src/api/products/products.js";
 import cartsRouter from "./src/api/carts/carts.js";
 import { __dirname } from "./utils.js";
+import handlebars from "express-handlebars";
 
 const port = 8080;
 
 const app = express();
+
+// Config de express-handlebars
+app.engine('handlebars', handlebars.engine());
+
+app.set('views', __dirname + '/src/views');
+
+app.set('view engine', 'handlebars');
 
 // Config de express
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +23,13 @@ app.use('/static', express.static(__dirname + '/public'));
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+app.get('/', (req,res) => {
+  let testUser = {
+    name: "Tom"
+  }
+  res.render('index', testUser);
+})
 
 // Product endpoints
 app.use("/products", productsRouter);
