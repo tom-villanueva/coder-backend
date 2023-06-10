b438af83af8a742dc86b219663b09198cf53ffd0
tom-villanueva <tomas.villanueva.jousset@gmail.com>
2023-06-09 22:33:00 -0300

    Product views with mongo

--- proyecto_final/src/routes/views/viewsProducts.js
+++ proyecto_final/src/routes/views/viewsProducts.js
@@ -1,5 +1,18 @@
 import express from "express";
+import productsService from "../../dao/services/products.service.js";
 
 const viewsProductsRouter = express.Router();
+
+viewsProductsRouter.get("/", async (req, res) => {
+  const products = await productsService.getAllProducts();
+  const context = {
+    products: products.map((product) => ({
+      title: product.title,
+      description: product.description,
+      price: product.price,
+    })),
+  };
+  res.render("index", { products: context.products });
+});
 
 export default viewsProductsRouter;
