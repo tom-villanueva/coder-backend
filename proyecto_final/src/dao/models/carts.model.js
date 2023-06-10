e99f9635117c4e395ff8f725edf03bd97073b70c
tom-villanueva <tomas.villanueva.jousset@gmail.com>
2023-06-09 21:57:37 -0300

    Cart CRUD with mongo

--- proyecto_final/src/dao/models/carts.model.js
+++ proyecto_final/src/dao/models/carts.model.js
@@ -1,8 +1,16 @@
 import { Schema, model } from "mongoose";
-import { ProductSchema } from "./products.model";
+
+const CartItemSchema = new Schema({
+  pid: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+  quantity: {
+    type: Number,
+    required: true,
+    min: [1, "Quantity can not be less then 1."],
+  },
+});
 
 const CartSchema = new Schema({
-  products: { type: [ProductSchema], required: true },
+  products: { type: [CartItemSchema], required: true },
 });
 
 const CartModel = model("carts", CartSchema);
--- proyecto_final/src/dao/services/carts.service.js
+++ proyecto_final/src/dao/services/carts.service.js
@@ -1,9 +1,72 @@
-import CartModel from "../dao/models/carts.model.js";
+import CartModel from "../models/carts.model.js";
+import { BadRequestError, NotFoundError, ServerError } from "../error.js";
 
 class CartService {
+  checkId(id) {
+    if (!id) {
+      throw new BadRequestError("Id required");
+    }
+  }
+
   async getAllCarts() {
-    const carts = await CartModel.find({});
-    return carts;
+    try {
+      const carts = await CartModel.find({});
+      return carts;
+    } catch (error) {
+      throw new ServerError(error);
+    }
+  }
+
+  async getCartProducts(id) {
+    try {
+      this.checkId(id);
+      const cart = await CartModel.findOne({ _id: id });
+
+      return cart.products;
+    } catch (error) {
+      throw new ServerError(error);
+    }
+  }
+
+  async createCart() {
+    try {
+      const cart = await CartModel.create({});
+      return cart;
+    } catch (error) {
+      throw new ServerError(error);
+    }
+  }
+
+  async addProductToCart(id, product) {
+    try {
+      let productAdded = await CartModel.findOneAndUpdate(
+        {
+          _id: id,
+          "products.pid": product._id,
+        },
+        {
+          $inc: {
+            "products.$.quantity": 1,
+          },
+        },
+        { new: true }
+      );
+
+      // if the product is a new one
+      if (!productAdded) {
+        productAdded = await CartModel.findOneAndUpdate(
+          {
+            _id: id,
+          },
+          { $push: { products: { pid: product._id, quantity: 1 } } },
+          { new: true }
+        );
+      }
+
+      return productAdded;
+    } catch (error) {
+      throw new ServerError(error);
+    }
   }
 }
 
