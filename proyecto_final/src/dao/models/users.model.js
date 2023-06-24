2587d54003f15735cfac9217753b6a1a9bd0930f
tom-villanueva <tomas.villanueva.jousset@gmail.com>
2023-06-24 13:03:34 -0300

    Update user model with age and password

--- proyecto_final/src/dao/models/users.model.js
+++ proyecto_final/src/dao/models/users.model.js
@@ -6,6 +6,8 @@ const UserModel = model(
     firstName: { type: String, required: true, max: 100 },
     lastName: { type: String, required: true, max: 100 },
     email: { type: String, required: true, max: 100 },
+    age: { type: Number, required: true },
+    password: { type: String, required: true },
   })
 );
 
