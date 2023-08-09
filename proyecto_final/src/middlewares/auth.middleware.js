758442ab9ecd991c39dbdf5ea93f102d9f97c0f3
tom-villanueva <tomas.villanueva.jousset@gmail.com>
2023-08-09 11:07:54 -0300

    Implemented isAdmin

--- proyecto_final/src/middlewares/auth.middleware.js
+++ proyecto_final/src/middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ export const isLoggedIn = (req, res, next) => {
 };
 
 export const isAdmin = (req, res, next) => {
-  if (req.session.user.role === "admin") {
+  if (req.session.user && req.session.user.role === "admin") {
     return next();
   }
 
